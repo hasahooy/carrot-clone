5f26dedf5a18c3707830cb7e858d9e697d93b3bf
han sanghoon <93518184+hasahooy@users.noreply.github.com>
2023-07-06 17:19:36 +0900

    회원 가입 로직 구현
    프론트엔드 및 백엔드 작업  signup

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ import sqlite3
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items(
+	id INTEGER PRIMARY KEY,
+	title TEXT NOT NULL,
+	image BLOB,
+	price INTEGER NOT NULL,
+	description TEXT,
+	place TEXT NOT NULL,
+	insertAt INTEGER NOT NULL
+            );
+                """)
+
+
 app = FastAPI()
 
 
@@ -49,5 +62,20 @@ async def get_image(item_id):
                              SELECT image from items WHERE id={item_id} 
                               """).fetchone()[0]
     return Response(content=bytes.fromhex(image_bytes))
- 
+
+@app.post('/signup')
+def signup(id:Annotated[str,Form()],
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]
+           ):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    print(id,password)
+    return '200' 
+
+
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
